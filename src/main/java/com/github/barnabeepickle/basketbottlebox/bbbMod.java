9bf0d7084e7d5a5dd1e7f6bfdab2f2511ddb3796
barnabeepickle <picklebarnabee@gmail.com>
2026-02-11 13:56:54 -0500

    created ModGUIHandler.java

--- src/main/java/com/github/barnabeepickle/basketbottlebox/bbbMod.java
+++ src/main/java/com/github/barnabeepickle/basketbottlebox/bbbMod.java
@@ -4,9 +4,11 @@ import com.github.barnabeepickle.basketbottlebox.client.ModClientHandler;
 import com.github.barnabeepickle.basketbottlebox.events.ModBlocks;
 import com.github.barnabeepickle.basketbottlebox.events.ModItems;
 import com.github.barnabeepickle.basketbottlebox.events.ModTileEntities;
+import com.github.barnabeepickle.basketbottlebox.networking.ModGUIHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,6 +22,8 @@ public class bbbMod {
 
     @Mod.EventHandler
     public void preLoadEvent(FMLPreInitializationEvent event) {
+        NetworkRegistry.INSTANCE.registerGuiHandler(INSTANCE, new ModGUIHandler());
+
         MinecraftForge.EVENT_BUS.register(ModBlocks.class);
         MinecraftForge.EVENT_BUS.register(ModTileEntities.class);
         MinecraftForge.EVENT_BUS.register(ModItems.class);
