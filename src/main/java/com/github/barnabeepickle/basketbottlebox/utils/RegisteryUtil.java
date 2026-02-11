10a00ca9b2ece9a9f159b85715ccbe41c1dac2cb
barnabeepickle <picklebarnabee@gmail.com>
2026-02-11 12:59:20 -0500

    added new utilities to RegisteryUtil.java

--- src/main/java/com/github/barnabeepickle/basketbottlebox/utils/RegisteryUtil.java
+++ src/main/java/com/github/barnabeepickle/basketbottlebox/utils/RegisteryUtil.java
@@ -1,7 +1,9 @@
 package com.github.barnabeepickle.basketbottlebox.utils;
 
 import com.github.barnabeepickle.basketbottlebox.Tags;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -12,5 +14,9 @@ public class RegisteryUtil {
 
     public static <V extends IForgeRegistryEntry<V>> void registerEntry(IForgeRegistry<V> registry, IForgeRegistryEntry<V> o, ResourceLocation name) {
         registry.register(o.setRegistryName(name));
+    }
+
+    public static void registerTileEntity(Class<? extends TileEntity> tileEntity, String name) {
+        GameRegistry.registerTileEntity(tileEntity, new ResourceLocation(Tags.MODID, name));
     }
 }
