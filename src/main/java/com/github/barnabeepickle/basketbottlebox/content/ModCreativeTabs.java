1466784014b6951121b4717450e7959f2a15ed9d
barnabeepickle <picklebarnabee@gmail.com>
2026-02-11 10:20:56 -0500

    created creative tab

---
+++ src/main/java/com/github/barnabeepickle/basketbottlebox/content/ModCreativeTabs.java
@@ -0,0 +1,8 @@
+package com.github.barnabeepickle.basketbottlebox.content;
+
+import com.github.barnabeepickle.basketbottlebox.content.tabs.ModPrimaryTab;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ModCreativeTabs {
+    public static final CreativeTabs primaryCreativeTab = new ModPrimaryTab();
+}
