2fdb0b1996b9a30b5dbae63d6aa2e0e1f1e2b950
barnabeepickle <picklebarnabee@gmail.com>
2026-02-12 11:03:19 -0500

    fixed ModItems not being on the event bus

--- src/main/java/com/github/barnabeepickle/ppnp/events/ModItems.java
+++ src/main/java/com/github/barnabeepickle/ppnp/events/ModItems.java
@@ -1,21 +1,19 @@
 package com.github.barnabeepickle.ppnp.events;
 
+import com.github.barnabeepickle.ppnp.content.items.ModBlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
-import static com.github.barnabeepickle.ppnp.content.ModCreativeTabs.primaryCreativeTab;
 import static com.github.barnabeepickle.ppnp.events.ModBlocks.RED_PRESENT;
 import static com.github.barnabeepickle.ppnp.utils.RegisteryUtil.registerEntry;
 
 public class ModItems {
-    public static ItemBlock ITEM_RED_PRESENT = new ItemBlock(RED_PRESENT);
+    public static ItemBlock ITEM_RED_PRESENT = new ModBlockItem(RED_PRESENT);
 
-    public ModItems() {
-        ITEM_RED_PRESENT.setCreativeTab(primaryCreativeTab);
-    }
-
+    @SubscribeEvent
     public static void registerItemsEvent(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> itemEvent = event.getRegistry();
 
