080a3b6ffe797dff9b5ee8b15b47a19cf295eb2b
barnabeepickle <picklebarnabee@gmail.com>
2026-02-12 10:15:32 -0500

    attempted to create the present slots at the right spot

--- src/main/java/com/github/barnabeepickle/ppnp/containers/PresentContainer.java
+++ src/main/java/com/github/barnabeepickle/ppnp/containers/PresentContainer.java
@@ -15,7 +15,7 @@ public class PresentContainer extends Container {
         // present inventory
         for (int row = 0; row < 2; row++) {
             for (int column = 0; column < 9; column++) {
-                this.addSlotToContainer(new Slot(invPresent, column + row * 9 + 9, 35 + column * 18, 117 + row * 18));
+                this.addSlotToContainer(new Slot(invPresent, column + row * 9 + 9, 6 + column * 18, 6 + row * 18));
             }
         }
 
