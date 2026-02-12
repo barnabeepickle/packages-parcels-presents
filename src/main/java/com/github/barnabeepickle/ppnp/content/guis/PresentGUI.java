94008ad61be0678e93f002738534d21b662deea4
barnabeepickle <picklebarnabee@gmail.com>
2026-02-12 11:21:51 -0500

    made PresentGUI draw textures

--- src/main/java/com/github/barnabeepickle/ppnp/content/guis/PresentGUI.java
+++ src/main/java/com/github/barnabeepickle/ppnp/content/guis/PresentGUI.java
@@ -3,7 +3,11 @@ package com.github.barnabeepickle.ppnp.content.guis;
 import com.github.barnabeepickle.ppnp.Tags;
 import com.github.barnabeepickle.ppnp.containers.PresentContainer;
 import com.github.barnabeepickle.ppnp.containers.inventory.PresentInventory;
+import com.github.barnabeepickle.ppnp.utils.ColorUtil;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 
@@ -21,6 +25,13 @@ public class PresentGUI extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+        GlStateManager.color(1F, 1F, 1F, 1F);
+        Minecraft.getMinecraft().renderEngine.bindTexture(texture);
+        this.drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+    }
 
+    @Override
+    protected void drawGuiContainerForegroundLayer(int var1, int var2) {
+        this.fontRenderer.drawString(I18n.format("container.present"), 6, 8, ColorUtil.getColor(64, 64, 64));
     }
 }
