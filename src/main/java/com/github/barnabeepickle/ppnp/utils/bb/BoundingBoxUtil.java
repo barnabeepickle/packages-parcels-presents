e4fafe904e9799937af067497bc387e1dfdc8734
barnabeepickle <picklebarnabee@gmail.com>
2026-02-27 10:06:08 -0500

    switched to using org.joml

--- src/main/java/com/github/barnabeepickle/ppnp/utils/bb/BoundingBoxUtil.java
+++ src/main/java/com/github/barnabeepickle/ppnp/utils/bb/BoundingBoxUtil.java
@@ -1,9 +1,8 @@
 package com.github.barnabeepickle.ppnp.utils.bb;
 
 import net.minecraft.util.math.AxisAlignedBB;
-
-import javax.vecmath.Matrix3d;
-import javax.vecmath.Vector3d;
+import org.joml.Matrix3d;
+import org.joml.Vector3d;
 
 public class BoundingBoxUtil {
     public static Vector3d rotHack(Vector3d vector, double angle) {
@@ -11,11 +10,11 @@ public class BoundingBoxUtil {
 
         // identity matrix
         Matrix3d rotMat = new Matrix3d(1,0,0, 0,1,0, 0,0,1);
-        rotMat.rotY(angle); // rotate on the y axis
+        rotMat.rotateY(angle); // rotate on the y axis
         rotMat.transform(vector);
 
-        vector.setX(Math.round(vector.getX() * 10000000D)/10000000D);
-        vector.setZ(Math.round(vector.getZ() * 10000000D)/10000000D);
+        vector.x = Math.round(vector.x() * 10000000D)/10000000D;
+        vector.z = Math.round(vector.z() * 10000000D)/10000000D;
 
         return vector;
     }
@@ -23,13 +22,13 @@ public class BoundingBoxUtil {
     public static AxisAlignedBB rotHackAABB(AxisAlignedBB aabb) {
         Vector3d minVec = rotHack(new Vector3d(aabb.minX, aabb.minY, aabb.minZ), 90);
         Vector3d maxVec = rotHack(new Vector3d(aabb.maxX, aabb.maxY, aabb.maxZ), 90);
-        if (minVec.getX() < 0) {
-            minVec.setX(Math.abs(minVec.getX() + 1));
-            maxVec.setX(Math.abs(maxVec.getX() + 1));
+        if (minVec.x() < 0) {
+            minVec.x = Math.abs(minVec.x() + 1);
+            maxVec.x = Math.abs(maxVec.x() + 1);
         }
-        if (minVec.getZ() < 0) {
-            minVec.setZ(Math.abs(minVec.getZ() + 1));
-            maxVec.setZ(Math.abs(maxVec.getZ() + 1));
+        if (minVec.z() < 0) {
+            minVec.z = Math.abs(minVec.z() + 1);
+            maxVec.z = Math.abs(maxVec.z() + 1);
         }
 
         return new AxisAlignedBB(minVec.x, minVec.y, minVec.z, maxVec.x, maxVec.y, maxVec.z);
